b5e47d929f884c5dd071066833ecceb45841af36
mlkir <mlkir@progresspoint.ru>
2012-06-19 20:31:05 +0400

    Создание проекта

---
+++ MobileTrips/T4TAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  T4TAppDelegate.h
+//  MobileTrips
+//
+//  Created by Maxim L. Kiryushin on 19.06.12.
+//  Copyright (c) 2012 Progress Solutions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface T4TAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MobileTrips/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MobileTrips
+//
+//  Created by Maxim L. Kiryushin on 19.06.12.
+//  Copyright (c) 2012 Progress Solutions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "T4TAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([T4TAppDelegate class]));
+    }
+}
